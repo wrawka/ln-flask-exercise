a072e1b3a267a576cfa1574cdbeb1a3b57690938
wrawka <wrawka@gmail.com>
2023-01-03 17:06:31 +0530

    [1] prototype

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello, Butt!'
+    context = {
+        'name': 'Patato'
+    }
+    return render_template('home.html', context=context)
 
 
-@app.route('/about')
-def about():
-    return 'This is a Flask learning app.'
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        context = request.form
+        return render_template('your_url.html', context=context)
+    return '☹️ This is not valid.'
